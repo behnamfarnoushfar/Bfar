b671c250a5eec32a42e0acfda7daba9c19c5edb5
Behnam.Farnoushfar <Behnam.Farnoushfar@pdexp.com>
2025-05-03 22:31:53 +0330

    +general response

--- XCutting/Abstraction/Infrastructures/IRepository.cs
+++ XCutting/Abstraction/Infrastructures/IRepository.cs
@@ -1,11 +1,13 @@
-﻿namespace Bfar.XCutting.Abstractions.Infrastructures
+﻿using Bfar.XCutting.Abstractions.Dtos;
+
+namespace Bfar.XCutting.Abstractions.Infrastructures
 {
     public interface IRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
-        Task AddAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(int id);
+        Task<DbGeneralResponse?> AddAsync(T entity);
+        Task<DbGeneralResponse?> UpdateAsync(T entity);
+        Task<DbGeneralResponse?> DeleteAsync(int id);
     }
 }
