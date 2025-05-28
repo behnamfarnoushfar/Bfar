3351fe73c70dcf9bce961d38d92a3f90883a1454
Behnam.Farnoushfar <Behnam.Farnoushfar@pdexp.com>
2025-05-28 22:52:05 +0330

    *interface method

--- XCutting/Abstraction/Adapters/IRestClientAdapter.cs
+++ XCutting/Abstraction/Adapters/IRestClientAdapter.cs
@@ -1,6 +1,10 @@
-﻿namespace Bfar.XCutting.Abstractions.Adapters
+﻿using Bfar.XCutting.Abstractions.Dtos;
+using Bfar.XCutting.Abstractions.Models;
+
+namespace Bfar.XCutting.Abstractions.Adapters
 {
     public interface IRestClientAdapter
     {
+        Task<GeneralResponse<TResponse?>> ExecuteJsonAsync<TResponse, TBody>(string host, string resource = "/", string method = "GET", TBody? body = null, List<NameValueModel> headers = null, List<NameValueModel> files = null, List<NameValueModel> formParameters = null, CancellationToken cancellationToken = default) where TBody : class;
     }
 }
